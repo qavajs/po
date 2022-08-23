bf683cfe73e14f43e325767be9b31fe3eb4365fd
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-08-23 17:01:31 +0300

    added component utility class

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const po = require('./src/PO');
 const { $, $$ } = require('./src/register');
+const Component = require('./src/Component');
 
 module.exports = {
     po,
     $,
-    $$
+    $$,
+    Component
 }
---
+++ src/Component.js
@@ -0,0 +1,15 @@
+/**
+ * Utility class that simplifies page object creation
+ */
+class Component {
+
+    /**
+     * @param {string} selector - component selector
+     */
+    constructor(selector) {
+        this.selector = selector;
+    }
+
+}
+
+module.exports = Component;
